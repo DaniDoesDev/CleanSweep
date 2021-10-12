35efe536bccf164693646291dca1dc7e09b05fda
Danielle <43801670+DaniDoesDev@users.noreply.github.com>
2021-10-12 12:07:10 -0500

    add testing for move methods

---
+++ src/RobotTest.java
@@ -0,0 +1,46 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class RobotTest {
+
+	@Test
+	void testMoveUp() {
+		Robot cleanSweep = new Robot();
+		if (cleanSweep.moveUp() == false) {
+			assertEquals(cleanSweep.coordinates, "1,1" );
+		} else {
+			assertEquals(cleanSweep.coordinates, "1,2" );
+		}
+	}
+	
+	@Test
+	void testMoveDown() {
+		Robot cleanSweep = new Robot();
+		if (cleanSweep.moveDown() == false) {
+			assertEquals(cleanSweep.coordinates, "1,1" );
+		} else {
+			assertEquals(cleanSweep.coordinates, "1,0" );
+		}
+	}
+	
+	@Test
+	void testMoveLeft() {
+		Robot cleanSweep = new Robot();
+		if (cleanSweep.moveLeft() == false) {
+			assertEquals(cleanSweep.coordinates, "1,1" );
+		} else {
+			assertEquals(cleanSweep.coordinates, "0,1" );
+		}
+	}
+	
+	@Test
+	void testMoveRight() {
+		Robot cleanSweep = new Robot();
+		if (cleanSweep.moveRight() == false) {
+			assertEquals(cleanSweep.coordinates, "1,1" );
+		} else {
+			assertEquals(cleanSweep.coordinates, "2,1" );
+		}
+	}
+
+}

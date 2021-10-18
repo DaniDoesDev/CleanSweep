ae4b3042009154c11c5614de669f58e66cf93646
sajithbandara <sajith.bandara2@gmail.com>
2021-10-17 19:18:34 -0500

    added basic surface level type detection based on the number of units required from the floor plan

---
+++ src/SurfaceLevel.java
@@ -0,0 +1,24 @@
+public class SurfaceLevel {
+    int SurfaceLevel;
+
+    public SurfaceLevel(int SurfaceLevel) {
+        this.SurfaceLevel = SurfaceLevel;
+    }
+
+    public enum FloorType {
+
+        BARE, LOW, HIGH, BLOCKED
+
+    }
+
+    public FloorType getSurfaceLevel(int SurfaceLevel) {
+        if (SurfaceLevel == 1)
+            return FloorType.BARE;
+        if (SurfaceLevel == 2)
+            return FloorType.LOW;
+        if (SurfaceLevel == 3)
+            return FloorType.HIGH;
+        else
+            return FloorType.BLOCKED;
+    }
+}

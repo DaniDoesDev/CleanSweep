d978d5c4b70eb93d85f8fe284b9b9be258658c94
Danielle <43801670+DaniDoesDev@users.noreply.github.com>
2021-10-25 15:49:41 -0500

    add method to drain battery while Clean Sweep traverses

--- src/Cleaner.java
+++ src/Cleaner.java
@@ -1,4 +1,5 @@
 import java.util.*;
+
 public class Cleaner {
 
     private static final double MAX_BATTERY_CAPACITY = 250;
@@ -48,6 +49,34 @@ public class Cleaner {
             System.out.println("My cleaner capacity is maxed out, please return me to the charging station and turn on my 'EMPTY ME' indicator");
         }
         
+    }
+
+    public void drainBatteryMovement(SurfaceLevel.FloorType curr, SurfaceLevel.FloorType destination) {
+        if (curr == destination) {
+            if (curr == SurfaceLevel.FloorType.BARE) {
+                currBattery -= 1;
+            } else if (curr == SurfaceLevel.FloorType.LOW) {
+                currBattery -= 2;
+            } else if (curr == SurfaceLevel.FloorType.HIGH) {
+                currBattery -= 3;
+            }
+            // Otherwise floor types are different, get the average
+        } else {
+            if ((curr == SurfaceLevel.FloorType.BARE || destination == SurfaceLevel.FloorType.BARE) 
+            && (curr == SurfaceLevel.FloorType.LOW || destination == SurfaceLevel.FloorType.LOW)) {
+                currBattery -= 1.5;
+            } else if ((curr == SurfaceLevel.FloorType.BARE || destination == SurfaceLevel.FloorType.BARE) 
+            && (curr == SurfaceLevel.FloorType.HIGH || destination == SurfaceLevel.FloorType.HIGH)) {
+                currBattery -= 2;
+            } else if ((curr == SurfaceLevel.FloorType.LOW || destination == SurfaceLevel.FloorType.LOW) 
+            && (curr == SurfaceLevel.FloorType.HIGH || destination == SurfaceLevel.FloorType.HIGH)) {
+                currBattery -= 2.5;
+            }
+        }
+        System.out.println("The battery level is currently at " + getCurrBattery() + " units of charge");
+        if (getCurrBattery() == 0) {
+            System.out.println("I have run out of power, not good!");
+        }
     }
 
 
